278fbaee7c1f8e864e4eae80e3ffe7f43455d32f
Tate Jones <tnj@njit.edu>
2025-10-24 16:31:24 -0400

    Chore: added style config file

---
+++ src/style_config.py
@@ -0,0 +1,9 @@
+STYLE = {
+    "primary_color": "#4F8A8B",
+    "secondary_color": "#FBD46D",
+    "accent_color": "#FF5959",
+    "background_color": "#F7F7F7",
+    "text_color": "#222831",
+    "header_font": "Montserrat, sans-serif",
+    "body_font": "Roboto, sans-serif"
+}
